8fc1ddc881180fb9c08fe7cc43b18490f0745be7
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-09 19:55:03 +0330

    1- Adding task.g.dart to data directory for generate

---
+++ lib/data/task.dart
@@ -0,0 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 1)
+class Task {
+  Task({required this.title, required this.subTitle});
+
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String subTitle;
+}
