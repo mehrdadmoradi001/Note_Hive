3313d57645421f929d73f5131a2379cc3890a143
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-09 18:05:55 +0330

    1- Creating Task Item

---
+++ lib/data/constants/constant_colors.dart
@@ -0,0 +1,6 @@
+  import 'dart:ui';
+
+  const grayColor = Color(0xffE5E5E5);
+  const whiteColor = Color(0xffffffff);
+  const greenColor = Color(0xff18DAA3);
+  const greenLightColor = Color(0xffE2F6F1);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_hive_flutter/data/constants/constant_colors.dart';
+import 'package:msh_checkbox/msh_checkbox.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,12 +10,136 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: grayColor,
       body: SafeArea(
-        child: Text('Mehrdad'),
+        child: Center(
+          child: _getTaskItem(),
+        ),
       ),
+    );
+  }
+
+  Widget _getTaskItem() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 18),
+      height: 132,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: _getMainContent(),
+      ),
+    );
+  }
+
+  Widget _getMainContent() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MSHCheckbox(
+                    size: 32,
+                    style: MSHCheckboxStyle.fillScaleCheck,
+                    colorConfig: MSHColorConfig.fromCheckedUncheckedDisabled(
+                      checkedColor: greenColor,
+                    ),
+                    value: isChecked,
+                    onChanged: (selected) {
+                      setState(() {
+                        isChecked = selected;
+                      });
+                    },
+                  ),
+                  Text('title'),
+                ],
+              ),
+              Text('SecondText'),
+              Spacer(),
+              _getTimeAndEditBadges(),
+            ],
+          ),
+        ),
+        SizedBox(width: 20,),
+        Image.asset(
+          'images/workout.png',
+        ),
+      ],
+    );
+  }
+
+  Widget _getTitle() {
+    return Column(
+      children: [
+        _getTimeAndEditBadges(),
+      ],
+    );
+  }
+
+  Row _getTimeAndEditBadges() {
+    return Row(
+      children: [
+        Container(
+          width: 90,
+          height: 28,
+          decoration: BoxDecoration(
+            color: greenColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 12,
+              vertical: 6,
+            ),
+            child: Row(
+              children: [
+                Text('10:30'),
+                SizedBox(width: 10),
+                Image.asset('images/icon_time.png'),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(width: 12),
+        Container(
+          width: 90,
+          height: 28,
+          decoration: BoxDecoration(
+            color: greenLightColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 12,
+            ),
+            child: Row(
+              children: [
+                Text(
+                  'ویرایش',
+                  style: TextStyle(color: greenColor),
+                ),
+                SizedBox(width: 10),
+                Image.asset('images/icon_edit.png'),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
