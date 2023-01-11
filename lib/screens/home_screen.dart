35feec3b70e669d480cd3add047adc96b2db19fd
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-11 17:23:10 +0330

    1- Hide FAB with NotificationListener

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_hive_flutter/data/constants/constant_colors.dart';
@@ -16,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var taskBox = Hive.box<Task>('taskBox');
+  bool isFabVisible = true;
 
   @override
   Widget build(BuildContext context) {
@@ -25,26 +27,43 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Center(
           child: ValueListenableBuilder(
             valueListenable: taskBox.listenable(),
-            builder: (context, value, child) => ListView.builder(
-              itemCount: taskBox.values.length,
-              itemBuilder: (context, index) {
-                var task = taskBox.values.toList()[index];
-                return TaskWidget(task: task);
-              },
-            ),
+            builder: (context, value, child) =>
+                NotificationListener<UserScrollNotification>(
+                  onNotification:(notif){
+                    setState(() {
+                      if(notif.direction == ScrollDirection.forward){
+                        isFabVisible = true;
+                      }
+                      if(notif.direction == ScrollDirection.reverse){
+                        isFabVisible = false;
+                      }
+                    });
+                    return true;
+                  },
+                  child: ListView.builder(
+                    itemCount: taskBox.values.length,
+                    itemBuilder: (context, index) {
+                      var task = taskBox.values.toList()[index];
+                      return TaskWidget(task: task);
+                    },
+                  ),
+                ),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => AddTaskScreen(), //
-            ),
-          );
-        },
-        backgroundColor: greenColor,
-        child: Image.asset('images/icon_add.png'),
+      floatingActionButton: Visibility(
+        visible: isFabVisible,
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => AddTaskScreen(), //
+              ),
+            );
+          },
+          backgroundColor: greenColor,
+          child: Image.asset('images/icon_add.png'),
+        ),
       ),
     );
   }
